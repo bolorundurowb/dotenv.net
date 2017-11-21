72ab2395e58f4a89992c5035888c328194fe49dd
Bolorunduro Winner-Timothy B <ogatimo@gmail.com>
2017-11-22 00:14:59 +0100

    add logic to write to environment variables

--- dotenv.net/DotEnv.cs
+++ dotenv.net/DotEnv.cs
@@ -23,6 +23,30 @@ namespace dotenv.net
             
             // split the long string into an array of rows
             string[] dotEnvRows = dotEnvContents.Split(new[] {"\r\n", Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries);
+            
+            // loop through rows, split into key and value then add to enviroment
+            foreach (var dotEnvRow in dotEnvRows)
+            {
+                string[] keyValue = dotEnvRow.Split(new[] {"="}, StringSplitOptions.None);
+                
+                // if the row is empty continue
+                if (keyValue.Length == 0) 
+                {
+                    continue;
+                }
+                
+                // if there is only a key add a null value
+                else if (keyValue.Length == 1)
+                {
+                    Environment.SetEnvironmentVariable(keyValue[0].Trim(), null);
+                }
+
+                // if the value exists then set it
+                else
+                {
+                    Environment.SetEnvironmentVariable(keyValue[0].Trim(), keyValue[1].Trim());
+                }
+            }
         }
     }
 }

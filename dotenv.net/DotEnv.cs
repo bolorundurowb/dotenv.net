543138c8ca6793bacb3cafbea108dda654943e01
Bolorunduro Winner-Timothy B <ogatimo@gmail.com>
2017-11-21 23:58:29 +0100

    add default encoding support

--- dotenv.net/DotEnv.cs
+++ dotenv.net/DotEnv.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 namespace dotenv.net
 {
     public class DotEnv
     {
-        public static void Config(bool throwOnError = true, string filePath = ".env")
+        public static void Config(bool throwOnError = true, string filePath = ".env", Encoding encoding = null)
         {
             if (throwOnError && !File.Exists(filePath))
             {
                 throw new FileNotFoundException("Environment file specified does not exist.");
             }
+
+            if (encoding == null)
+            {
+                encoding = Encoding.Default;
+            }
             
+            string dotEnvContents = File.ReadAllText(filePath, )
         }
     }
 }

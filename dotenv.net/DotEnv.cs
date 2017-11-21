be9eefc499f3d6510527d9f876abeeeedff4ceb0
Bolorunduro Winner-Timothy B <ogatimo@gmail.com>
2017-11-21 23:47:18 +0100

    add scaffold

---
+++ dotenv.net/DotEnv.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace dotenv.net
+{
+    public class DotEnv
+    {
+        public static void Config(bool throwOnError = true, string filePath = ".env")
+        {
+            if (throwOnError)
+            {
+                if (!File.Exists(filePath))
+                {
+                    throw new FileNotFoundException("Environment file specified does not exist.");
+                }
+            }
+        }
+    }
+}

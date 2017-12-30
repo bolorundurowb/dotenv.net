1f0e17d64450a3732dc84c43da2c4e398dae0a6a
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2017-12-30 19:13:11 +0100

    feature: add the service collection extensions

---
+++ dotenv.net/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace dotenv.net.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddEnv(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}

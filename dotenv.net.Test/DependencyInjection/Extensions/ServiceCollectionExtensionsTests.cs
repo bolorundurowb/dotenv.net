614cb44fdfdd42ae55a787ac22c6e9c07768d465
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-03-25 19:16:52 +0100

    ft: add two add env tests

---
+++ dotenv.net.Test/DependencyInjection/Extensions/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using dotenv.net.DependencyInjection.Extensions;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace dotenv.net.Test.DependencyInjection.Extensions
+{
+    public class ServiceCollectionExtensionsTests
+    {
+        [Fact]
+        public void ShouldThrowWhenServicesAreNull()
+        {
+            Action action = () => ServiceCollectionExtensions.AddEnv(null, builder => { });
+            action.Should()
+                .ThrowExactly<ArgumentNullException>();
+        }
+        
+        [Fact]
+        public void ShouldThrowWhenSetupActionIsNull()
+        {
+            var services = new ServiceCollection();
+            Action action = () => ServiceCollectionExtensions.AddEnv(services, null);
+            action.Should()
+                .ThrowExactly<ArgumentNullException>();
+        }
+    }
+}

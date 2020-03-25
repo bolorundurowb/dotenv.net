a9b7291f18d826fc2706f4eb5b380e17035c3372
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-03-25 19:20:55 +0100

    ft: finalize add env tests

--- dotenv.net.Test/DependencyInjection/Extensions/ServiceCollectionExtensionsTests.cs
+++ dotenv.net.Test/DependencyInjection/Extensions/ServiceCollectionExtensionsTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using dotenv.net.DependencyInjection.Extensions;
 using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
@@ -23,6 +24,30 @@ namespace dotenv.net.Test.DependencyInjection.Extensions
             Action action = () => ServiceCollectionExtensions.AddEnv(services, null);
             action.Should()
                 .ThrowExactly<ArgumentNullException>();
+        }
+        
+        [Fact]
+        public void ShouldWorkWhenSetupActionIsValid()
+        {
+            var services = new ServiceCollection();
+            Action action = () => ServiceCollectionExtensions.AddEnv(services, options =>
+            {
+                options.AddEnvFile(".env");
+                    options.AddEncoding(Encoding.UTF8);
+                    options.AddThrowOnError(false);
+                });
+            action.Should()
+                .NotThrow<Exception>();
+        }
+        
+        [Fact]
+        public void ShouldWorkWithAutomatedOptions()
+        {
+            var services = new ServiceCollection();
+            Action action = () => ServiceCollectionExtensions.AddEnv(services);
+            // NOTE: this is because the default file does not exist
+            action.Should()
+                .NotThrow<Exception>();
         }
     }
 }

4306c2a11252606db39ab311125601119e79273c
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-03-27 08:53:37 +0100

    ch: whitespace fixes

--- dotenv.net/DependencyInjection/Extensions/AutoFacExtension.cs
+++ dotenv.net/DependencyInjection/Extensions/AutoFacExtension.cs
@@ -8,15 +8,15 @@ namespace dotenv.net.DependencyInjection.Extensions
 {
     public static class AutoFacExtension
     {
-        public static ContainerBuilder AddEnv(this ContainerBuilder containerBuilder, 
+        public static ContainerBuilder AddEnv(this ContainerBuilder containerBuilder,
             Action<DotEnvOptionsBuilder> action)
         {
-            if(containerBuilder == null)
+            if (containerBuilder == null)
             {
                 throw new ArgumentNullException(nameof(containerBuilder));
             }
 
-            if(action == null)
+            if (action == null)
             {
                 throw new ArgumentNullException(nameof(action));
             }
@@ -40,9 +40,9 @@ namespace dotenv.net.DependencyInjection.Extensions
             AddEnv(containerBuilder, ac =>
             {
                 ac.AddEnvFile(".env")
-                  .AddEncoding(Encoding.UTF8)
-                  .AddThrowOnError(true)
-                  .AddTrimOptions(true);
+                    .AddEncoding(Encoding.UTF8)
+                    .AddThrowOnError(true)
+                    .AddTrimOptions(true);
             });
 
             return containerBuilder;

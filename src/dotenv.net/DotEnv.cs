9bf258d532c52da07d560a0bec8b4a0cbb23f790
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-03-15 17:11:34 +0100

    ft: add helper methods

--- src/dotenv.net/DotEnv.cs
+++ src/dotenv.net/DotEnv.cs
@@ -27,6 +27,49 @@ namespace dotenv.net
             }
         }
 
+        private static ReadOnlySpan<KeyValuePair<string, string>> ReadAndParse(string envFilePath,
+            bool ignoreExceptions, Encoding encoding, bool trimValues)
+        {
+            var rawEnvRows = Reader.Read(envFilePath, ignoreExceptions, encoding);
+
+            if (rawEnvRows == ReadOnlySpan<string>.Empty)
+            {
+                return ReadOnlySpan<KeyValuePair<string, string>>.Empty;
+            }
+
+            return Parser.Parse(rawEnvRows, trimValues);
+        }
+
+        private static IDictionary<string, string> ReadAndReturn(string envFilePath,
+            bool ignoreExceptions, Encoding encoding, bool trimValues)
+        {
+            var response = new Dictionary<string, string>();
+            var envRows = ReadAndParse(envFilePath, ignoreExceptions, encoding, trimValues);
+            foreach (var envRow in envRows)
+            {
+                if (response.ContainsKey(envRow.Key))
+                {
+                    response[envRow.Key] = envRow.Value;
+                }
+                else
+                {
+                    response.Add(envRow.Key, envRow.Value);
+                }
+            }
+
+            return response;
+        }
+
+        private static void ReadAndWrite(string envFilePath,
+            bool ignoreExceptions, Encoding encoding, bool trimValues)
+        {
+            var envRows = ReadAndParse(envFilePath, ignoreExceptions, encoding, trimValues);
+            foreach (var envRow in envRows)
+            {
+                Environment.SetEnvironmentVariable(envRow.Key, envRow.Value);
+            }
+        }
+
         /// <summary>
         /// Configure the environment variables from a .env file
         /// </summary>

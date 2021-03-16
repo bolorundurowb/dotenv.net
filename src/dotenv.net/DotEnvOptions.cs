5d39610d85a99cc5302156cb79f99eaa15c2df54
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-03-16 13:11:16 +0100

    ft: update the dotenv options

--- src/dotenv.net/DotEnvOptions.cs
+++ src/dotenv.net/DotEnvOptions.cs
@@ -6,45 +6,56 @@ namespace dotenv.net
     public class DotEnvOptions
     {
         private const string DefaultEnvFileName = ".env";
+        private const int DefaultProbeDepth = 4;
 
         /// <summary>
-        /// A value to state whether to throw an exception if the env file doesn't exist. The default is true. <see cref="T:dotenv.net.DotEnvOptions"/>
+        /// A value to state whether to throw or swallow exceptions. The default is true. <see cref="T:dotenv.net.DotEnvOptions"/>
         /// </summary>
         public bool IgnoreExceptions { get; set; }
 
         /// <summary>
-        /// The paths to the env files. <see cref="T:dotenv.net.DotEnvOptions"/>
+        /// The paths to the env files. The default is [.env] <see cref="T:dotenv.net.DotEnvOptions"/>
         /// </summary>
         public IEnumerable<string> EnvFilePaths { get; set; }
 
         /// <summary>
-        /// The Encoding that the env file was created with. <see cref="T:dotenv.net.DotEnvOptions"/>
+        /// The Encoding that the env file was created with. The default is UTF-8. <see cref="T:dotenv.net.DotEnvOptions"/>
         /// </summary>
         public Encoding Encoding { get; set; }
 
         /// <summary>
-        /// A value to state whether or not to trim whitespace from the values retrieved. <see cref="T:dotenv.net.DotEnvOptions"/>
+        /// A value to state whether or not to trim whitespace from the values retrieved. The default is true. <see cref="T:dotenv.net.DotEnvOptions"/>
         /// </summary>
         public bool TrimValues { get; set; }
 
         /// <summary>
-        /// A value to state whether or not to override the env variable if it has been set. <see cref="T:dotenv.net.DotEnvOptions"/>
+        /// A value to state whether or not to override the env variable if it has been set. the default is true. <see cref="T:dotenv.net.DotEnvOptions"/>
         /// </summary>
-        public bool OverrideExistingVariables { get; set; }
+        public bool AllowOverwriting { get; set; }
 
         /// <summary>
-        /// A value to state whether we traverse up the directory structure. <see cref="T:dotenv.net.DotEnvOptions"/>
+        /// A value to state whether we traverse up the directory structure. The default is false. <see cref="T:dotenv.net.DotEnvOptions"/>
         /// </summary>
-        public bool ProbePath { get; set; }
+        public bool ProbeForEnv { get; set; }
 
-        public DotEnvOptions()
+        /// <summary>
+        /// Default constructor for the dot env options
+        /// </summary>
+        /// <param name="ignoreExceptions"></param>
+        /// <param name="envFilePaths"></param>
+        /// <param name="encoding"></param>
+        /// <param name="trimValues"></param>
+        /// <param name="allowOverwriting"></param>
+        /// <param name="probeForEnv"></param>
+        public DotEnvOptions(bool ignoreExceptions = true, IEnumerable<string> envFilePaths = null,
+            Encoding encoding = null, bool trimValues = false, bool allowOverwriting = true, bool probeForEnv = false)
         {
-            IgnoreExceptions = true;
-            EnvFilePaths = new[] {DefaultEnvFileName};
-            Encoding = Encoding.UTF8;
-            TrimValues = true;
-            OverrideExistingVariables = true;
-            ProbePath = false;
+            IgnoreExceptions = ignoreExceptions;
+            EnvFilePaths = envFilePaths ?? new[] {DefaultEnvFileName};
+            Encoding = encoding ?? Encoding.UTF8;
+            TrimValues = trimValues;
+            AllowOverwriting = allowOverwriting;
+            ProbeForEnv = probeForEnv;
         }
     }
 }

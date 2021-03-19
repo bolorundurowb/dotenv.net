77332a71b80d8e0a4170cd1c163bfa343f044777
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-03-19 14:35:34 +0100

    ft: allow for not providing an options file

--- src/dotenv.net/DotEnv.cs
+++ src/dotenv.net/DotEnv.cs
@@ -43,18 +43,18 @@ namespace dotenv.net
         /// </summary>
         /// <param name="options">The options required to configure the env loader</param>
         /// <returns>The key value pairs read from the env files</returns>
-        public static IDictionary<string, string> Read(DotEnvOptions options)
+        public static IDictionary<string, string> Read(DotEnvOptions options = null)
         {
-            return Helpers.ReadAndReturn(options);
+            return Helpers.ReadAndReturn(options ?? new DotEnvOptions());
         }
 
         /// <summary>
         /// Load the values in the provided env files into the environment variables
         /// </summary>
         /// <param name="options">The options required to configure the env loader</param>
-        public static void Load(DotEnvOptions options)
+        public static void Load(DotEnvOptions options = null)
         {
-            Helpers.ReadAndWrite(options);
+            Helpers.ReadAndWrite(options ?? new DotEnvOptions());
         }
     }
 }
